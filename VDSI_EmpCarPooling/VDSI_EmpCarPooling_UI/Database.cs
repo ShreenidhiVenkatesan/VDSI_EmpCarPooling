9c164450f6c09e6f0e3414fd959b736ccf63b77c
ShreenidhiVenkatesan <shreenidhi90@gmail.com>
2015-08-27 18:05:23 +0530

    database

--- VDSI_EmpCarPooling/VDSI_EmpCarPooling_UI/Database.cs
+++ VDSI_EmpCarPooling/VDSI_EmpCarPooling_UI/Database.cs
@@ -2,10 +2,79 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace VDSI_EmpCarPooling
 {
+ 
     public class Database
     {
+        
+        
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Connection"].ConnectionString);
+
+        DataSet ds = new DataSet();
+    
+        public DataSet SelectData(String Query)
+        {
+            try
+            {
+                
+                SqlCommand cmd = new SqlCommand(Query,con);
+              
+                SqlDataAdapter da = new SqlDataAdapter(Query,con);
+                da.Fill(ds);
+                
+
+               
+            }
+            catch (Exception e)
+            {
+            }
+            return ds;
+
+        }
+
+        public void InsertandupdateData(string query)
+        {
+            SqlCommand cmd=new SqlCommand(query,con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            
+        }
+
+        public void Storedprocwithparameter(string spname,string[] paramenter)
+        {
+
+            SqlCommand cmd=new SqlCommand(spname,con);
+            con.Open();
+             cmd.CommandType=CommandType.StoredProcedure;
+            foreach(string data in paramenter)
+            {
+                cmd.Parameters.Add(data);
+            }
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        public void storedproc(string spname)
+        {
+            
+            SqlCommand cmd=new SqlCommand(spname,con);
+            con.Open();
+             cmd.CommandType=CommandType.StoredProcedure;
+             cmd.ExecuteNonQuery();
+            con.Close();
+        }
     }
+    
+    }
+
+  
+    
 }
