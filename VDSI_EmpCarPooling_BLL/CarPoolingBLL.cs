7a19088a99f68255fa57c377d0e8e63e85d9bc32
ShreenidhiVenkatesan <shreenidhi90@gmail.com>
2015-08-27 15:04:07 +0530

    test code
    test code

--- VDSI_EmpCarPooling_BLL/CarPoolingBLL.cs
+++ VDSI_EmpCarPooling_BLL/CarPoolingBLL.cs
@@ -10,11 +10,14 @@ namespace VDSI_EmpCarPooling_BLL
     public class CarPoolingBLL
     {
 
-        //public void getLoginDetails(string username, string password)
-        //{
-        //     = new VDSI_CARPEntities();            
-        //    var loginList = from l in LOGIN.
-        //                     where 
-        //}
+        public void getLoginDetails(string username, string password)
+        {
+            //VDSI_CARPEntities entities  = new VDSI_CARPEntities();
+            //var loginList = (from list in entities.LOGINs
+            //                 select list);
+            //bool valid = loginList.ToList().Exists(a => a.UserName.Equals(username) && a.Password.Equals(password));
+
+                            
+        }
     }
 }
