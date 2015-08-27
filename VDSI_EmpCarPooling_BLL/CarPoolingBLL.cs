1ab04cb48bc92b1a7dc1e126547aed0562becfc0
ShreenidhiVenkatesan <shreenidhi90@gmail.com>
2015-08-27 14:38:23 +0530

    Entity Framework
    Entity Framework

---
+++ VDSI_EmpCarPooling_BLL/CarPoolingBLL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VDSI_EmCarPooling_DAL;
+
+namespace VDSI_EmpCarPooling_BLL
+{
+    public class CarPoolingBLL
+    {
+
+        //public void getLoginDetails(string username, string password)
+        //{
+        //    VDSI_CARPEntities carPooling = new VDSI_CARPEntities();            
+        //    var loginList = from l in LOGIN.
+        //                     where 
+        //}
+    }
+}
